f36dcf0913d9e2868668e7ebb87828d1657d567b
CloneNikita <120546590+CloneNikita@users.noreply.github.com>
2024-06-11 16:30:24 +0500

    Update ClashBallsInfiniteParry.lua

--- ClashBallsInfiniteParry.lua
+++ ClashBallsInfiniteParry.lua
@@ -1,19 +1,34 @@
+local lastanim = nil
+local lastchange = nil
 local anims = {"rbxassetid://14948120460","rbxassetid://14948115850","rbxassetid://14956791372","rbxassetid://14956788249","rbxassetid://14956777030"}
 function distance(x,y)
 return (x.Position - y.Position).Magnitude
 end
 function run()
 local anim = anims[math.random(1,#anims)]
+if anim ~= lastanim then
+lastanim = anim
+local speed = game.ReplicatedStorage.VA.AnimSpeed
 local animationInstance = Instance.new("Animation")
 animationInstance.AnimationId = anim
 local animationTrack = game:GetService("Players").LocalPlayer.Character.Humanoid:LoadAnimation(animationInstance)
+animationTrack:AdjustSpeed(speed.Value)
 animationTrack:Play()
-end
-local player = game.Players.LocalPlayer
-game.Workspace.EffectStorage.ChildAdded:Connect(function(v)
-if v:IsA("BasePart") and distance(game:GetService("Players").LocalPlayer.Character:WaitForChild("HumanoidRootPart"), v) <= 13.5 then
+else
 run()
 end
+end
+local player = game.Players.LocalPlayer
+game.ReplicatedStorage.VA.TargetPlayer.Changed:Connect(function()
+if lastchange.Name == player.Name and lastchange ~= nil then
+run()
+else
+if lastchange ~= nil then
+print("Before: "..lastchange.Name)
+print("Now: "..game.ReplicatedStorage.VA.TargetPlayer.Value.Name)
+end
+end
+lastchange = game.ReplicatedStorage.VA.TargetPlayer.Value
 end)
 while true do
 game:GetService("ReplicatedStorage"):WaitForChild("RE"):WaitForChild("BattingEvent"):FireServer(false)
