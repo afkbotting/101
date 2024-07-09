31892a6931696de16f961ce0843e83080baebd71
CloneNikita <120546590+CloneNikita@users.noreply.github.com>
2024-07-09 12:46:41 +0500

    Create DOORSLOLHAXSOLARA.lua

---
+++ DOORSLOLHAXSOLARA.lua
@@ -0,0 +1,5 @@
+game:GetService("StarterGui"):SetCore("SendNotification",{
+	Title = "Low End PC ERROR: Solara", -- Required
+	Text = "Ошибка, ваш ПК слишком слаб для использования чита.", -- Required
+	Duration = 99999
+})
